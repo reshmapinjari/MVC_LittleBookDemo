3df7921c665d94b84e6fafaf5043afbdce1edf97
saleempinjari <pinjarisaleem8032@gmail.com>
2020-09-02 17:32:05 +0530

    initial commit

---
+++ Todo_Mvc/Todo_Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Todo_Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Todo_Mvc/Todo_Mvc/Services/ITodoItemService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Todo_Mvc.Models;
+
+namespace Todo_Mvc.Services
+{
+    public interface ITodoItemService
+    {
+        
+            Task<TodoItem[]> GetIncompleteItemsAsync();
+        
+    }
+}
